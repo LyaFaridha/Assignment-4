8f35c9fd2025bce948a73457a1682a8ec65dbc14
LyaFaridha <143927970+LyaFaridha@users.noreply.github.com>
2023-09-12 00:44:23 +0800

    Update AlyaProject.py

--- AlyaProject.py
+++ AlyaProject.py
@@ -21,43 +21,98 @@ fig_country_counts.update_layout(
     title={'text': 'Distribution of Country of Origins', 'x': 0.5}
 )
 
-# Define the app layout
-app.layout = html.Div([
-    html.H1("Data Visualization"),
-
-    dcc.RadioItems(
-        id='radio-graph',
-        options=[
-            {'label': 'Country of Origin', 'value': 'country-counts'},
-            {'label': 'Aroma vs. Flavor', 'value': 'aroma-flavor'},
-            {'label': 'Processing Method', 'value': 'processing-method'},
-        ],
-        value='country-counts',
-        labelStyle={'display': 'block'}
-    ),
-
-    dcc.Graph(id='graph-output'),
-
-])
-
-# Define callback to update the graph based on radio button selection
-@app.callback(
-    Output('graph-output', 'figure'),
-    Input('radio-graph', 'value')
+app.layout = html.Div(
+    [html.H1("Data Visualization"),
+     dcc.RadioItems(id='my-radio', options=['Country Counts'], value='Country Counts', inline=True),
+     dcc.Graph(id='graph-output', figure={})]
 )
-def update_graph(radio_value):
-    if radio_value == 'country-counts':
+
+@app.callback(
+    Output(component_id='graph-output', component_property='figure'),
+    Input(component_id='my-radio', component_property='value')
+)
+def update_my_graph(val_chosen):
+    if val_chosen == 'Country Counts':
         return fig_country_counts
-    elif radio_value == 'aroma-flavor':
-        # Replace with the appropriate scatter plot for aroma vs. flavor
-        # You can reuse your existing fig1
-        return fig1
-    elif radio_value == 'processing-method':
-        # Replace with the appropriate box plot for processing methods
-        # You can reuse your existing fig2
-        return fig2
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+This code loads the data from the provided URL and creates a bar chart (fig_country_counts) based on the 'Country of Origin' counts. The Dash app allows you to select "Country Counts" from the radio button, and it will display the bar chart dynamically based on the data from the URL.
 
 
+
+
+User
+delete the radio items, updategraph, and app callback so just showthe graph
+ChatGPT
+If you want to remove the radio items, update graph functionality, and the callback, and simply display the bar chart, you can simplify the code as follows:
+
+python
+Copy code
+from dash import Dash, html
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+server = app.server
+
+app.title = "MCM7003 Data Visualization Interactive Demo"
+
+# Load data from the provided URL
+data_url = "https://raw.githubusercontent.com/LyaFaridha/Assignment3/main/df_arabica_clean.csv"
+df = pd.read_csv(data_url)
+
+# Create a bar chart for the 'Country of Origin' counts
+country_counts = df['Country of Origin'].value_counts()
+fig_country_counts = px.bar(country_counts, x=country_counts.index, y=country_counts.values)
+
+fig_country_counts.update_layout(
+    xaxis_title='Country of Origin',
+    yaxis_title='Count',
+    title={'text': 'Distribution of Country of Origins', 'x': 0.5}
+)
+
+app.layout = html.Div(
+    [html.H1("Data Visualization"),
+     dcc.Graph(id='graph-output', figure=fig_country_counts)]
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+This simplified code removes the radio items and the callback, so the app will only display the bar chart without any interactivity.
+
+
+
+
+User
+pls add this graphs "# Relationship between Country of Origin and Aroma/Flavor Attributes
+fig = px.scatter(df, x='Aroma', y='Flavor', color='Country of Origin',
+                 labels={'Aroma': 'Aroma', 'Flavor': 'Flavor', 'Country of Origin': 'Country of Origin'},
+                 title='Relationship between Aroma and Flavor Attributes by Country of Origin')
+fig.update_traces(
+    marker=dict(size=10),
+    selector=dict(mode='markers')
+)" and "fig = px.box(df, x='Processing Method', y=['Aroma', 'Flavor'],
+             labels={'variable': 'Attribute', 'value': 'Rating', 'Processing Method': 'Processing Method'},
+             title='Comparison of Aroma and Flavor Attributes across Processing Methods')
+fig.show()"
+ChatGPT
+To add the two additional graphs to your Dash application, you can modify the code as follows:
+
+python
+Copy code
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+server = app.server
+
+app.title = "MCM7003 Data Visualization Interactive Demo"
+
+# Load data from the provided URL
+data_url = "https://raw.githubusercontent.com/LyaFaridha/Assignment3/main/df_arabica_clean.csv"
+df = pd.read_csv(data_url)
 
 # Create the first scatter plot
 fig1 = px.scatter(df, x='Aroma', y='Flavor', color='Country of Origin',
@@ -73,83 +128,17 @@ fig2 = px.box(df, x='Processing Method', y=['Aroma', 'Flavor'],
              labels={'variable': 'Attribute', 'value': 'Rating', 'Processing Method': 'Processing Method'},
              title='Comparison of Aroma and Flavor Attributes across Processing Methods')
 
-# Define the app layout
-app.layout = html.Div([
-    html.H1("Data Visualization"),
-
-    dcc.Tabs(id='tabs', value='tab-1', children=[
-        dcc.Tab(label='Bar Chart', value='tab-1'),
-        dcc.Tab(label='Scatter Plot', value='tab-2'),
-        dcc.Tab(label='Box Plot', value='tab-3'),
-    ]),
-
-    html.Div(id='tabs-content'),
-
-    html.Br(),
-
-    dcc.RadioItems(
-        id='radio-graph',
-        options=[
-            {'label': 'Country of Origin', 'value': 'country-counts'},
-            {'label': 'Aroma vs. Flavor', 'value': 'aroma-flavor'},
-            {'label': 'Processing Method', 'value': 'processing-method'},
-        ],
-        value='country-counts',
-        labelStyle={'display': 'block'}
-    ),
-
-    html.Br(),
-
-    dcc.Checklist(
-        id='checklist',
-        options=[
-            {'label': 'Show Grid', 'value': 'grid'},
-            {'label': 'Show Lines', 'value': 'lines'},
-        ],
-        value=['grid', 'lines']
-    )
-])
-
-# Define callback to update graphs and beautify them
-@app.callback(
-    Output('tabs-content', 'children'),
-    Output('graph-output', 'figure'),
-    Output('graph1-output', 'figure'),
-    Output('graph2-output', 'figure'),
-    Input('tabs', 'value'),
-    Input('radio-graph', 'value'),
-    Input('checklist', 'value')
-)
-def update_graph(tab, radio_value, checklist_value):
-    fig = None
-    if tab == 'tab-1':
-        fig = fig_country_counts
-    elif tab == 'tab-2':
-        fig = fig1
-    elif tab == 'tab-3':
-        fig = fig2
-
-    # Customize the appearance based on checklist values
-    for value in checklist_value:
-        if value == 'grid':
-            fig.update_xaxes(showgrid=True)
-            fig.update_yaxes(showgrid=True)
-        elif value == 'lines':
-            fig.update_xaxes(showline=True, linewidth=1, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=1, linecolor='black')
-
-    return html.Div([
-        dcc.Graph(id='graph-output', figure=fig),
+app.layout = html.Div(
+    [
+        html.H1("Data Visualization"),
         dcc.Graph(id='graph1-output', figure=fig1),
         dcc.Graph(id='graph2-output', figure=fig2)
-    ]), fig_country_counts, fig1, fig2
+    ]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-This code adds tabs for different graphs, radio buttons to switch between them, and checkboxes to customize their appearance (show/hide grid and lines). You can further customize the appearance and functionality according to your requirements.
-
-
-
+In this updated code, I've added the two additional graphs (fig1 and fig2) to the Dash application. The first graph (fig1) is a scatter plot showing the relationship between Aroma and Flavor Attributes by Country of Origin. The second graph (fig2) is a box plot comparing Aroma and Flavor Attributes across Processing Methods. Both graphs are displayed in the Dash app.
 
 
 
